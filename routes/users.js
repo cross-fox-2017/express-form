b23b3e5205570b52eaf7fb39a92f1a1858f923a7
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-23 11:07:57 +0700

    release 2 done

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
+  let test = req.query.input.toLowerCase()
+  //---RELEASE 1-----
   // if(req.query && req.query.say)
-  res.send(req.query.say);
+  // res.send(req.query.say);
   // res.render('index', {title: 'irwin'})
-  // res.render('render')
+  res.render('render', {respond: test})
 });
 
+// router.post('/', function (res, req, next) {
+//   req.send('index')
+// })
 module.exports = router;
