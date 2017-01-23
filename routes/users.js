3e880e380371fc39714a06df8e838a5d51700533
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-23 11:39:19 +0700

    release 3 done

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let test = req.query.input.toLowerCase()
   //---RELEASE 1-----
   // if(req.query && req.query.say)
   // res.send(req.query.say);
   // res.render('index', {title: 'irwin'})
-  res.render('render', {respond: test})
+  res.render('render', {respond: ""})
 });
 
-// router.post('/', function (res, req, next) {
-//   req.send('index')
-// })
+router.post('/', function (req, res, next) {
+  if (req.body.input) {
+    var test = req.body.input.toLowerCase()
+  }
+  res.render('render', {respond: test})
+})
+
 module.exports = router;
