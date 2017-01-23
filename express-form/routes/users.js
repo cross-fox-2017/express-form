9555863d448b3f06211a122d86b4f7b96318f94f
Isumi <isumi.karina72@gmail.com>
2017-01-23 16:38:41 +0700

    Revisi

--- express-form/routes/users.js
+++ express-form/routes/users.js
@@ -7,6 +7,7 @@ var router = express.Router();
 // });
 
 router.get('/', function(req, res, next) {
+  var temp = req.query.say
   if(temp === undefined) {
     res.render('index', { title: 'HACKTIV8 FORM', answer: req.query.say});
   } else {
